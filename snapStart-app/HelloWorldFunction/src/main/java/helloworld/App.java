bc0a52a949415940e66733abdd66b5674e77d9f7
hungran <26101787+hungran@users.noreply.github.com>
2023-01-05 08:59:21 +0700

    init

---
+++ snapStart-app/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -0,0 +1,57 @@
+package helloworld;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.security.SecureRandom;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.logs.AWSLogsClient;
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+            final int outofsnapStart = this.outofsnapStart();
+            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" , \"secure_random\": \"%s\", \"secure_random_outofHandler\": \"%s\"}", pageContents, rng.nextInt(), outofsnapStart);
+            return response
+                .withStatusCode(200)
+                .withBody(output);
+        } catch (IOException e) {
+            return response
+                    .withBody("{}")
+                    .withStatusCode(500);
+        }
+    }
+
+    private String getPageContents(String address) throws IOException{
+        URL url = new URL(address);
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+
+    private static SecureRandom rng = new SecureRandom();
+
+    private AWSLogsClient logs;
+    public int outofsnapStart() throws IOException {
+        SecureRandom abc = new SecureRandom();
+        return abc.nextInt();
+    }
+}
