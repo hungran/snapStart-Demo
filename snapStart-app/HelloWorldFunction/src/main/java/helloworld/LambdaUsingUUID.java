4e72f6ca212a501ab96a1e068483bb13a01ef64e
hungran <26101787+hungran@users.noreply.github.com>
2023-01-05 18:20:26 +0700

    temp remove lambdausingUUID

--- snapStart-app/HelloWorldFunction/src/main/java/helloworld/LambdaUsingUUID.java
+++ snapStart-app/HelloWorldFunction/src/main/java/helloworld/LambdaUsingUUID.java
@@ -1,19 +1,19 @@
-import java.util.UUID;
-import com.amazonaws.services.AWSLogsClient;
-public class LambdaUsingUUID {
+// import java.util.UUID;
+// import com.amazonaws.services.AWSLogsClient;
+// public class LambdaUsingUUID {
 
-   private AWSLogsClient logs;
-   private final UUID sandboxId;
+//    private AWSLogsClient logs;
+//    private final UUID sandboxId;
 
-   public LambdaUsingUUID() {
-      sandboxId = UUID.randomUUID(); // <-- unique content created
-      logs = new AWSLogsClient();
-   }
-   @Override
-   public String handleRequest(Map<String,String> event, Context context) {
-      CreateLogStreamRequest request = new CreateLogStreamRequest(
-        "myLogGroup", sandboxId + ".log9.txt");
-        logs.createLogStream(request);     
-        return "Hello world!";
-   }
-} 
+//    public LambdaUsingUUID() {
+//       sandboxId = UUID.randomUUID(); // <-- unique content created
+//       logs = new AWSLogsClient();
+//    }
+//    @Override
+//    public String handleRequest(Map<String,String> event, Context context) {
+//       CreateLogStreamRequest request = new CreateLogStreamRequest(
+//         "myLogGroup", sandboxId + ".log9.txt");
+//         logs.createLogStream(request);     
+//         return "Hello world!";
+//    }
+// } 
